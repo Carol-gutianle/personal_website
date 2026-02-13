5e8a92403a332580dfed2f352e4fb195a8558046
Carol-gutianle <68144157+Carol-gutianle@users.noreply.github.com>
2026-02-13 11:25:43 +0000

    Deploying to gh-pages from @ Carol-gutianle/Carol-gutianle.github.io@eb9d87781b80f892b077091943edf07958bafc8b 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "Carol-gutianle/personal_website",
+    "data-repo": "Carol-gutianle/Carol-gutianle.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
